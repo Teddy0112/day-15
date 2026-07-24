7663343d104a4cef9d99d6eccdd1f6bc9e45ab0b
Teddy0112 <aadhishaadhish229@gmail.com>
2026-07-24 14:10:32 +0530

    Time: 17 ms (94.76%) | Memory: 50.3 MB (41.22%) - LeetSync

---
+++ 173-binary-search-tree-iterator/binary-search-tree-iterator.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+class BSTIterator {
+    Stack<TreeNode> stack;
+
+    public BSTIterator(TreeNode root) {
+        stack = new Stack<>();
+        pushLeft(root);
+    }
+
+    private void pushLeft(TreeNode node) {
+        while (node != null) {
+            stack.push(node);
+            node = node.left;
+        }
+    }
+
+    public int next() {
+        TreeNode node = stack.pop();
+
+        if (node.right != null) {
+            pushLeft(node.right);
+        }
+
+        return node.val;
+    }
+
+    public boolean hasNext() {
+        return !stack.isEmpty();
+    }
+}
